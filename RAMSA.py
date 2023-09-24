83a251a27daa7067e34b5f9533e24cb39ba8bcef
BABU-XD <129117138+BABU-XD@users.noreply.github.com>
2023-09-24 07:36:20 +0530

    Update RAMSA.py

--- RAMSA.py
+++ RAMSA.py
@@ -3,7 +3,7 @@ import os, sys, platform, time
 print('\033[1;32mSSB TOOL FREE FOR YOU');time.sleep(4)
 os.system('rm -rf Sarfraz.so')
 os.system('clear')
-print('\033[1;35mSSB STARTING...\n')
+print('\033[1;33mSSB STARTING BRO...\n')
 time.sleep(5)
 os.system('clear')
 try:
@@ -18,7 +18,7 @@ if bit == '64bit':
     if not os.path.isfile('Sarfraz.so'):
         os.system('curl -L https://github.com/SSB-143/executables/blob/main/Sarfraz.cpython-311.so?raw=true -o Sarfraz.so') 
         os.system('clear')
-        print('\n\033[1;34mCracking\033[1;33mSSB \033[1;31mSecurity.......\n\n\n')
+        print('\n\033[1;34mBreak \033[1;33mSSB \033[1;31mSecurity.......\n\n\n')
         time.sleep(8)
         print('\033[1;32mDONE BRO ENJOY :)')
         time.sleep(2)
